76e6c4006f1e58ced1986ae02113015f79cf4f58
Akshay Raina <akshayraina@Akshays-MacBook-Pro.local>
2024-05-29 16:23:17 +0530

    ADD allowed hosts (*)

--- To_Do_List/settings.py
+++ To_Do_List/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$%z))fr7vjove#ws)w554q1i1ggfzxah35ufj+s&^g9ex%s8yu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
